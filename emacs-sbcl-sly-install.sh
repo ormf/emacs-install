2fd42b9d0e766fb84b6536b66832d7ba369beaab
ormf <junk14@selma.hfmdk-frankfurt.de>
2023-12-26 15:04:53 +0100

    misc.

--- emacs-sbcl-sly-install.sh
+++ emacs-sbcl-sly-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt install -y sbcl emacs
-sbcl --load /tmp/quicklisp.lisp
+sbcl --load /tmp/emacs-install/quicklisp.lisp
 cp /tmp/emacs-install/.sbclrc ~/
 cp -R /tmp/emacs-install/.emacs.d ~/
 emacs &
